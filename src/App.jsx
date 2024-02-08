8a6c8b15f76c75a1c3f93ad2472915a573ea8f55
ghada <ghadaemad011@gmail.com>
2024-02-09 00:58:34 +0200

    adding some pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 // import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-
+import Shelter from "./pages/Shelter/Shelter";
 import Login from "./pages/Login/Login";
-import Contacts from "./components/Contacts/Contacts";
+import Food from "./pages/Food/Food";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Resources from "./pages/Resources/Resources";
+import Education from "./pages/Education/Education";
+import Feed from "./pages/Feed/Feed";
+import Profile from "./pages/Profile/Profile";
+import Settings from "./pages/Settings/Settings";
+import Logout from "./pages/Logout/Logout";
 
 function App() {
   return (
@@ -15,10 +20,19 @@ function App() {
         {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+
           <Route path="/login" element={<Login />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/dashboard"element={<Dashboard/>}/>
+
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="shelter" element={<Shelter />} />
+            <Route path="food" element={<Food />} />
+            <Route path="resources" element={<Resources/>}/>
+            <Route path="education" element={<Education/>}/>
+            <Route path="feed" element={<Feed/>}/>
+            <Route path="profile" element={<Profile/>}/>
+            <Route path="settings" element={<Settings/>}/>
+            <Route path="logout" element={<Logout/>}/>
+          </Route>
         </Routes>
       </Router>
     </>
