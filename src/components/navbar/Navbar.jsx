84488ff6b490727d905805e312d5f05536990020
ghada <ghadaemad011@gmail.com>
2024-02-12 16:30:07 +0200

    edit navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,38 +8,66 @@ import Navbar_Mobile from "./Navbar_Mobile/Navbar_Mobile";
 const Navbar = () => {
   const [OpenMenu, setOpenMenu] = useState(false);
   return (
-    <nav className="bg-light sticky z-40 top-0 pr-4  w-full flex sm:block justify-between items-center">
-    <div>
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+    <nav className="flex sticky top-0 z-40 bg-light text-basic px-6 py-2 max-w-[1536px] mx-auto  items-center justify-between">
+      {/* image and title */}
+      <div className="flex gap-4 items-center">
+        <img src={Logo} alt="Logo Image" />
+        <h1 className="font-bold text-lg hidden sm:block">
+          HOME C<span className="text-Orange">O</span>MPASS
+        </h1>
+      </div>
+
+      <div className="hidden md:block">
+        <ul className="flex gap-4 font-bold text-lg cursor-pointer">
+          <AnchorLink
+            href="#home"
+            className="hover:text-Orange ease-in-out transition-all duration-150"
+          >
+            Home
+          </AnchorLink>
+          <AnchorLink
+            href="#about"
+            className="hover:text-Orange ease-in-out transition-all duration-150"
+          >
+            About us
+          </AnchorLink>
+          <AnchorLink
+            href="#contacts"
+            className="hover:text-Orange ease-in-out transition-all duration-150"
+          >
+            Contact us
+          </AnchorLink>
+        </ul>
+      </div>
+      <div className="hidden sm:flex items-center justify-center gap-6">
+        <Link
+          className="bg-light hover:bg-basic hover:text-light  px-6 py-1 font-bold text-sm border-4 text-basic rounded-lg border-basic "
+          to="/login"
+        >
+          Sign in
+        </Link>
+        <Link
+          className="bg-basic  text-light px-6 py-1 rounded-lg hover:text-basic hover:bg-light font-bold text-sm border-4 hover:border-basic"
+          to="/signup"
+        >
+          Sign Up
+        </Link>
+      </div>
+
+      {OpenMenu ? (
         <div>
-          <img src={Logo} alt="home-campas" />
+          <Navbar_Mobile setOpenMenu={setOpenMenu}/>
         </div>
-
-        <div className="space-x-6 font-semibold text-lg hidden sm:flex text-basic justify-end">
-          <AnchorLink href='#home'>Home</AnchorLink>
-         
-           <AnchorLink href="#about">About</AnchorLink>
-           <AnchorLink href="#contacts">Contact</AnchorLink>
-           <Link to='/login'>Login</Link>
-          
+      ) : (
+        <div className="block sm:hidden">
+          <HiOutlineMenu
+            size={30}
+            className="font-bold text-basic cursor-pointer"
+            onClick={() => setOpenMenu((prev) => !prev)}
+          />
         </div>
-      </div>
-    </div>
-
-    {OpenMenu ? (
-      <div>
-        <Navbar_Mobile setOpenMenu={setOpenMenu} />
-      </div>
-    ) : (
-      <div>
-        <HiOutlineMenu
-          size={35}
-          className="cursor-pointer text-basic block sm:hidden"
-          onClick={() => setOpenMenu((prev) => !prev)}
-        />
-      </div>
-    )}
-  </nav>
+      )}
+    </nav>
   );
 };
 
--- src/components/navbar/Navbar_Mobile/Navbar_Mobile.jsx
+++ src/components/navbar/Navbar_Mobile/Navbar_Mobile.jsx
@@ -3,26 +3,25 @@ import { Link } from "react-router-dom";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navbar_Mobile = ({ setOpenMenu }) => {
-
-
   return (
     <div className="block sm:hidden fixed right-0 bottom-0 z-40 h-full w-[300px] bg-light drop-shadow-md">
       <div className="flex justify-end p-4">
         <button>
           <IoClose
-            size={50}
-            className="block sm:hidden cursor-pointer"
+            size={30}
+            className="block sm:hidden mt-4 cursor-pointer"
             onClick={() => setOpenMenu((prev) => !prev)}
           />
         </button>
       </div>
 
       <div>
-        <div className="flex flex-col gap-4 pl-4">
-          <AnchorLink href="#about">
-            About</AnchorLink>
-          <AnchorLink href='#contacts'>Contact</AnchorLink>
-          <Link to='/login'>Login</Link>
+        <div className="flex font-semibold flex-col gap-8 pt-6 items-center justify-center">
+          <AnchorLink className="hover:text-Orange" href="#home">Home</AnchorLink>
+          <AnchorLink className="hover:text-Orange" href="#about">About</AnchorLink>
+          <AnchorLink className="hover:text-Orange" href="#contacts">Contact</AnchorLink>
+          <Link to="/login" className="bg-basic text-light px-8 py-2 rounded-lg">Login</Link>
+          <Link to="/signup" className="bg-light border-4 border-basic px-8 py-2 rounded-lg">Sign Up</Link>
         </div>
       </div>
     </div>
