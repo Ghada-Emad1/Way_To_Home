f31c8cede59f85de06198a1fad73ae2ba77eec04
ghada <ghadaemad011@gmail.com>
2024-02-13 17:33:18 +0200

    adding slider data

---
+++ src/data/slider.js
@@ -0,0 +1,28 @@
+export const sliderData=[
+    {
+        img:"/WorkPlaces.png",
+        Name:"Work Places",
+        description:"15 Ways to grow your saas in 21 days with no money"
+    },
+    {
+        img:'/shelter.png',
+        Name:"Shelter",
+        description:"Why everyone is moving today subscribitons"
+    },
+    {
+        img:"/restuarant.png",
+        Name:"Restaurant",
+        description:"9 ways to make it quick and never look back again in your life"
+    },
+    {
+        img:'/food.jpg',
+        Name:"Foods",
+        description:"Services like Foods to All of Homeless People"
+    },
+    {
+        img:'/resources.jpg',
+        Name:"Resources",
+        description:'How we Collect Our Data '
+
+    }
+]
