36cf4f46df148499a4ca08b36b505527deb5bf91
Ghada Emad <ghadaemad011@gmail.com>
2024-02-26 01:45:25 +0200

    add chatbot to right side of nav

--- src/components/SideNav/RightNav.jsx
+++ src/components/SideNav/RightNav.jsx
@@ -1,11 +1,30 @@
-
-
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+import { Link } from "react-router-dom";
 const RightNav = () => {
-    return (
-      <div className="w-1/4 bg-light">
-        RightBar
+  return (
+    <div className="w-1/4 bg-light text-basic font-semibold shadow-lg flex justify-center pt-10">
+      <div className="flex flex-col gap-6">
+        <div className="bg-white rounded-xl shadow-lg w-[150px]">
+          <Link to='/dashboard/chatbot'>
+            <button className="flex items-center p-4">
+              <IoChatbubbleEllipsesOutline className="mr-4"/>
+              <span>ChatBot</span>
+            </button>
+          </Link>
+        </div>
+
+        <div className="bg-white rounded-xl shadow-lg w-[150px]">
+          <Link to='/dashboard/search'>
+            <button className="flex items-center p-4">
+              <IoIosSearch className="mr-4"/>
+              <span>Search</span>
+            </button>
+          </Link>
+        </div>
       </div>
-    )
-  }
-  
-  export default RightNav
+    </div>
+  );
+};
+
+export default RightNav;
