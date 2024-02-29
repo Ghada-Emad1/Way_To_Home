aecf448dc84d7d06fe6c93aa48a9f02c3d8a673a
Ghada Emad <ghadaemad011@gmail.com>
2024-02-29 19:18:50 +0200

    add feed details

---
+++ src/data/FeedList.js
@@ -0,0 +1,11 @@
+export const FeedList=[
+    {
+        id:1,
+        img:"/Ellipse7.png",
+        name:"Unknown",
+        description:"Needy",
+        post:"I need a home , I live under Dort bridge."
+    },
+    
+   
+]
