f137ccdfe1cd0be71dd1789a1282c0e35afaecbe
Ghada Emad <ghadaemad011@gmail.com>
2024-03-01 21:40:44 +0200

    adding homelessDashboard details

---
+++ src/data/HomelessDasboard.js
@@ -0,0 +1,23 @@
+export const HomelessDashboard = [
+  {
+    img: "/shelterdashboard.png",
+    name: "Shelter",
+    link: "/dashboard/shelter",
+  },
+  {
+    img: "/foodDashboard.png",
+    name: "Foods",
+    link: "/dashboard/food",
+  },
+  {
+    img: "/workDashboard.png",
+    name: "Work",
+    link: "/dashboard/works",
+  },
+  {
+    img: "/postsDashboard.png",
+    name: "Posts",
+    link: "/dashboard/feed",
+  },
+  
+];
