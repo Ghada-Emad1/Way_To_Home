52be1024b41003fb9afda9f94d5da8789d5cce24
Ghada Emad <ghadaemad011@gmail.com>
2024-04-07 17:34:32 +0200

    add food page

--- src/pages/Food/Food.jsx
+++ src/pages/Food/Food.jsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router-dom";
-import { FoodList } from "../../data/FoodList";
-import { FaWhatsapp } from "react-icons/fa6";
-//import "./style.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Food = () => {
+  const[foodlist,setfoodlist]=useState([])
+  function GetFood() {
+    axios
+      .get("https://homecompassapi.azurewebsites.net/Facility/bycategory/10")
+      .then((res) => {
+        setfoodlist(res.data)
+      });
+  }
+  useEffect(() => {
+    GetFood();
+  }, []);
   return (
     <div className="bg-white  flex flex-col overflow-auto h-[100vh] text-nowrap">
       <div className="flex gap-5 justify-center my-4">
@@ -17,31 +27,37 @@ const Food = () => {
         </button>
       </div>
       <div className=" grid grid-cols-2 justify-center items-center gap-4 mx-4 bg-white">
-        {FoodList.map((element, index) => (
-          <div key={index} className=" content flex justify-center   gap-2 max-sm:flex flex-row ">
+        {foodlist.map((element, index) => (
+          <div
+            key={index}
+            className="bg-light p-4 content flex justify-center   gap-2 max-sm:flex flex-row "
+          >
             <div>
-            <div>
-              <img
-                className="w-[200px] h-[150px] object-cover rounded-lg"
-                src={element.img}
-                alt=""
-              />
-            </div>
-            <div className="flex flex-col gap-2 ">
-              <h1>
-                <span className="text-basic font-bold">Restaurant:</span>{" "}
-                {element.Name}
-              </h1>
-              <p>
-                <span className="text-basic font-bold">Address: </span>
-                {element.Address}
-              </p>
-              <div>
-                <button className="bg-basic rounded-lg text-white flex items-center gap-2 px-6 py-2">
-                  <FaWhatsapp /> {element.contact}
-                </button>
+              <div className="flex justify-center mb-4">
+                <img
+                  className="w-[200px] h-[150px] object-cover rounded-sm "
+                  src={element.photoUrl}
+                  alt=""
+                />
               </div>
-            </div>
+              <div className="flex flex-col gap-2 items-center">
+                <h1>
+                  <span className="text-basic font-bold">Restaurant:</span>{" "}
+                  {element.name}
+                </h1>
+               
+                <div className="flex text-wrap px-5">
+                <small className="w-[200px] text-Orange">{element.description}</small>
+
+                </div>
+                
+                <p><span className="font-bold text-basic">Location: </span>{element.location}</p>
+
+                <div>
+                  <p><span className="text-Orange font-bold">For: </span>{element.target}</p>
+                  
+                </div>
+              </div>
             </div>
           </div>
         ))}
@@ -51,5 +67,9 @@ const Food = () => {
     </div>
   );
 };
+
+  
+  
+  
 
 export default Food;
